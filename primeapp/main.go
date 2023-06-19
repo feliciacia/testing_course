e1b19ff9467325c5ad63e11cb67acbd42338332d
羅翡幸 <feliciaalo08@gmail.com>
2023-06-19 14:32:40 +0800

    user_can_input_manually

--- primeapp/main.go
+++ primeapp/main.go
@@ -1,11 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	n := 2
-	_, msg := isPrime(n)
-	fmt.Println(msg)
+	intro()
+
+	doneChan := make(chan bool)
+
+	go readUserInput(doneChan)
+
+	<-doneChan
+}
+
+func readUserInput(doneChan chan bool) {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		res, done := checkNumbers(scanner)
+
+		if done {
+			doneChan <- true
+			return
+		}
+		fmt.Println(res)
+		prompt()
+	}
+}
+
+func checkNumbers(scanner *bufio.Scanner) (string, bool) {
+	scanner.Scan()
+	if strings.EqualFold(scanner.Text(), "q") {
+		return "", true
+	}
+	numToCheck, err := strconv.Atoi(scanner.Text())
+
+	if err != nil {
+		return "Please enter a whole number!", false
+	}
+
+	_, msg := isPrime(numToCheck)
+	return msg, false
+}
+
+func intro() {
+	fmt.Println("Is it Prime?")
+	fmt.Println("------------")
+	fmt.Println("Enter a whole number, and we'll tell you if it is a prime number or not. Enter q to quit")
+	prompt()
+}
+
+func prompt() {
+	fmt.Print("-> ")
 }
 
 func isPrime(n int) (bool, string) {
