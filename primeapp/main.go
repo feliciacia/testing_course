66a4dc7b96aca6f9381a35c14972cf2e2a45ab99
羅翡幸 <feliciaalo08@gmail.com>
2023-06-19 10:52:46 +0800

    prime_testing

---
+++ primeapp/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	n := 2
+	_, msg := isPrime(n)
+	fmt.Println(msg)
+}
+
+func isPrime(n int) (bool, string) {
+
+	if n == 0 || n == 1 {
+		return false, fmt.Sprintf("%d is not a prime number", n)
+	}
+
+	if n < 0 {
+		return false, "negative numbers are not prime number"
+	}
+
+	for i := 2; i <= n/2; i++ {
+		if n%i == 0 {
+			return false, fmt.Sprintf("%d is not a prime number because visible by %d", n, i)
+		}
+	}
+
+	return true, fmt.Sprintf("%d is a prime number", n)
+}
