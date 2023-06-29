adcd1e75257d0a08f7f1c1b7e9a5d5389e078739
羅翡幸 <feliciaalo08@gmail.com>
2023-06-29 15:31:09 +0800

    create middleware for handle and extract client's ip address

---
+++ webapp/routes/middleware.go
@@ -0,0 +1,52 @@
+package routes
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+)
+
+type ContextKey string
+
+const ContextUserKey ContextKey = "user_ip"
+
+func (app *Application) ipFromContext(ctx context.Context) string {
+	return ctx.Value(ContextUserKey).(string)
+}
+
+func (app *Application) addIPToContext(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var ctx = context.Background()
+		ip, err := getIP(r)
+		if err != nil {
+			ip, _, _ = net.SplitHostPort(r.RemoteAddr)
+			if len(ip) == 0 {
+				ip = "unknown"
+			}
+			ctx = context.WithValue(r.Context(), ContextUserKey, ip)
+		} else {
+			ctx = context.WithValue(r.Context(), ContextUserKey, ip)
+		}
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func getIP(r *http.Request) (string, error) {
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		return "unknown", err
+	}
+	userIP := net.ParseIP(ip)
+	if userIP == nil {
+		return "", fmt.Errorf("userip : %q is not IP:port", r.RemoteAddr)
+	}
+	forward := r.Header.Get("X-Forwarded-For")
+	if len(forward) > 0 {
+		ip = forward
+	}
+	if len(forward) == 0 {
+		ip = "forward"
+	}
+	return ip, nil
+}
